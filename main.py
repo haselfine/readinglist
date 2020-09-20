8b74492483f24baaddc4c8427cd35c8c732d1260
yu2519hn <1234568hH*>
2020-09-20 18:01:23 -0500

    Added delete_book function

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def create_menu():
     menu.add_option('4', 'Show Read Books', show_read_books)
     menu.add_option('5', 'Show All Books', show_all_books)
     menu.add_option('6', 'Change Book Read Status', change_read)
+    menu.add_option('7', 'Delete Book', delete_book)
     menu.add_option('Q', 'Quit', quit_program)
 
     return menu
@@ -64,7 +65,20 @@ def change_read():
     new_read = ui.get_read_value()     
     book.read = new_read 
     book.save()
-    
+
+
+def delete_book():
+    try:
+        book_id = ui.get_book_id()
+        book = store.get_book_by_id(book_id)
+        answer = ui.ask_question("Type delete to confirm book deletion: ")
+        if answer == "delete":
+            book.delete(book)
+        else:
+            ui.message("No changes made")
+    except:
+        print('Error, No books deleted')
+
 
 def quit_program():
     ui.message('Thanks and bye!')
